dc71cfeda07b39fa18b95015b357f21fe02dfc20
Firefly <firefly.lzh@gmail.com>
2023-05-16 08:31:16 +0800

    format code
    Signed-off-by: Firefly <firefly.lzh@gmail.com>

--- eventpage.js
+++ eventpage.js
@@ -1,27 +1,47 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message['flag'] == 'openTable') {
-    chrome.tabs.query({currentWindow: true}, tabs => {
+  if (message["flag"] == "openTable") {
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
       // console.log(tabs)
       // console.log(message);
-      if (typeof message['url'] == "string") {
+      if (typeof message["url"] == "string") {
         for (const tab of tabs.reverse()) {
           if (tab.hasOwnProperty("openerTabId") || tab.active == true) {
-            chrome.tabs.create({index: tab.index + 1, url: message['url'], openerTabId: tab.id, active: message['active']});
+            chrome.tabs.create({
+              index: tab.index + 1,
+              url: message["url"],
+              openerTabId: tab.id,
+              active: message["active"],
+            });
             return;
           }
         }
       } else {
         for (const tab of tabs.reverse()) {
           if (tab.hasOwnProperty("openerTabId") || tab.active == true) {
-            for (const i in message['url']){
-              if (message['active'] == true){
-                if (i == 0){
-                  chrome.tabs.create({ index: tab.index + Number(i) + 1, url: message['url'][i]['url'], openerTabId: tab.id, active: message['active'] });
+            for (const i in message["url"]) {
+              if (message["active"] == true) {
+                if (i == 0) {
+                  chrome.tabs.create({
+                    index: tab.index + Number(i) + 1,
+                    url: message["url"][i]["url"],
+                    openerTabId: tab.id,
+                    active: message["active"],
+                  });
                 } else {
-                  chrome.tabs.create({ index: tab.index + Number(i) + 1, url: message['url'][i]['url'], openerTabId: tab.id, active: false });
+                  chrome.tabs.create({
+                    index: tab.index + Number(i) + 1,
+                    url: message["url"][i]["url"],
+                    openerTabId: tab.id,
+                    active: false,
+                  });
                 }
               } else {
-                chrome.tabs.create({ index: tab.index + Number(i) + 1, url: message['url'][i]['url'], openerTabId: tab.id, active: message['active'] });
+                chrome.tabs.create({
+                  index: tab.index + Number(i) + 1,
+                  url: message["url"][i]["url"],
+                  openerTabId: tab.id,
+                  active: message["active"],
+                });
               }
             }
             return;
@@ -29,13 +49,16 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         }
       }
     });
-  } else if (message['flag'] == 'download') {
-    chrome.downloads.download({
-      url: message['url'],
-      saveAs: message['saveAs']
-    },function(downloadId) {
+  } else if (message["flag"] == "download") {
+    chrome.downloads.download(
+      {
+        url: message["url"],
+        saveAs: message["saveAs"],
+      },
+      function (downloadId) {
         console.log(downloadId);
-    });
+      }
+    );
   }
-  sendResponse({status: 'ok'});
+  sendResponse({ status: "ok" });
 });
